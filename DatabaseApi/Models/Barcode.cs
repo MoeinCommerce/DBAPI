d382666b210bbd57b64a18b7ef4c44dffb7c856a
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-09-28 17:02:25 +0330

    add Barcode model

---
+++ DatabaseApi/Models/Barcode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseApi.Models
+{
+    public class Barcode
+    {
+        public int ID { get; set; }
+        public Nullable<int> ProductID { get; set; }
+        public string Text { get; set; }
+    }
+}
