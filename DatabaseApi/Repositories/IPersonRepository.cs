2488a2c145bd1a96fb146e77e4d6411e60e322b2
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-10-01 11:33:46 +0330

    add methods into IPersonRepository

--- DatabaseApi/Repositories/IPersonRepository.cs
+++ DatabaseApi/Repositories/IPersonRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,18 @@ namespace DatabaseApi.Repositories
 {
     public interface IPersonRepository
     {
+        void Add(Person person);
+        void AddRange(IEnumerable<Person> persons);
+        void AddPersonPhoneNumber(int personId, List<PersonPhoneNumber> phoneNumbers);
+        IEnumerable<Person> GetAll();
+        IEnumerable<Person> GetWhere(Expression<Func<Person, bool>> predicate);
+        Person GetPerson(int personId);
+        IEnumerable<PersonPhoneNumber> GetPersonPhoneNumbers(int personId);
+        IEnumerable<Person> GetPersonsByGroup(int groupId);
+        void Update(Person person);
+        void BulkUpdate(List<Person> persons);
+        void Delete(int personId, bool forceDelete = false);
+        void DeletePersonsByGroup(int groupId, bool forceDelete = false);
+        void DeleteAll(bool forceDelete = false);
     }
 }
