c36efa5ba630ffd027f60ac82089abdd83ba7c0a
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-10-01 11:31:54 +0330

    add two model PersonPhoneNumber, ProductStock

---
+++ DatabaseApi/Models/PersonPhoneNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseApi.Models
+{
+    public class PersonPhoneNumber
+    {
+        public int ID { get; set; }
+        public Nullable<int> PersonId { get; set; }
+        public string Caption { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ DatabaseApi/Models/ProductStock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseApi.Models
+{
+    public class ProductStock
+    {
+        public int ProductId { get; set; }
+        public double Stock { get; set; }
+    }
+}
