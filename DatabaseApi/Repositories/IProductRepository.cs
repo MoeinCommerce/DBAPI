45a5d041b64f96c6b87247aae9384662c643258f
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-10-01 11:33:28 +0330

    add new methods into IProductRepository

--- DatabaseApi/Repositories/IProductRepository.cs
+++ DatabaseApi/Repositories/IProductRepository.cs
@@ -16,5 +16,8 @@ namespace DatabaseApi.Repositories
         Product GetById(int id);
         void SetInitialStock(int productId, int wareHouseId, float stock);
         void Update(Product entity);
+        IEnumerable<ProductBarcode> GetProductBarcodes(int productId);
+        int GetProductStock(int productId, int warehouseId = 0);
+        List<ProductStock> GetAllProductStock(int warehouseId = 0);
     }
 }
