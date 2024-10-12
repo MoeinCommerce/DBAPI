1489c598c1be3308da0c05a0e13638dd25ff409e
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-10-12 11:47:06 +0330

    create interface IMoeinCommerceTablesRepository for creating tables of MoeinCommerce

---
+++ DatabaseApi/Repositories/IMoeinCommerceTablesRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseApi.Repositories
+{
+    public interface IMoeinCommerceTablesRepository
+    {
+        void CreateAllTables();
+        void CreatePaymentMethodMapTable();
+        void CreateCategoryMapTable();
+        void CreateCustomerMapTable();
+        void CreateWebCategoryTable();
+        void CreateProductMapTable();
+        void CreateInvoiceMapTable();
+        void CreateWebProductTable();
+    }
+}
