f12960f4156eadd6e8f125c355dfee9ded8fb8f1
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-10-13 16:09:19 +0330

    add new methods to IInvoiceRepository

--- DatabaseApi/Repositories/IInvoiceRepository.cs
+++ DatabaseApi/Repositories/IInvoiceRepository.cs
@@ -9,5 +9,14 @@ namespace DatabaseApi.Repositories
 {
     public interface IInvoiceRepository
     {
+        void Add(Invoice invoice, List<InvoiceProductItem> invoiceProductItems);
+        void Add(Invoice invoice, List<InvoiceServiceItem> invoiceServiceItems);
+        void AddRange(Tuple<Invoice, List<InvoiceProductItem>> invoices);
+        void AddRange(Tuple<Invoice, List<InvoiceServiceItem>> invoices);
+        void ChangeInvoiceOwnership(Invoice invoice, Person person);
+        void Delete(Invoice invoice);
+        void Update(Invoice invoice);
+        void AddInvoiceProductItems(List<InvoiceProductItem> invoiceProductItems);
+        void AddInvoiceServiceItems(List<InvoiceServiceItem> invoiceServiceItems);
     }
 }
