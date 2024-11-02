3cefd4786111309d99799ccf1c8e8aac1d3ff4d9
Erfan4708 <abbasy.4708@gmail.com>
2024-11-02 12:06:17 +0330

    Modify InvoiceModel and Create InvoiceItem for storing details of invoices

---
+++ DatabaseApi/Models/MappableModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseApi.Models
+{
+    public class MappableModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? WebId { get; set; }
+        public string WebName { get; set; }
+        public int? TargetWeb { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public bool UpdateRequired { get; set; }
+    }
+}
---
+++ DatabaseApi/Models/McModels/McEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseApi.Models.McModels
+{
+    public class McEntity
+    {
+        public int id { get; set; }
+        public int wcid { get; set; }
+        public int target_web { get; set; }
+        public DateTime last_update { get; set; }
+        public bool update_required { get; set; }
+    }
+}
