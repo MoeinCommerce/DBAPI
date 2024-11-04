af920fc84f21b1bdac1c0e22a80af334a8d5e33c
Erfan4708 <abbasy.4708@gmail.com>
2024-11-04 10:44:59 +0330

    Create a DbConfig class to store the configuration created in relation to the database

---
+++ DatabaseApi/Models/DbConfig.cs
@@ -0,0 +1,9 @@
+namespace DatabaseApi.Models
+{
+    public class DbConfig
+    {
+        public string Key { get; set; }
+        public string DefaultValue { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
