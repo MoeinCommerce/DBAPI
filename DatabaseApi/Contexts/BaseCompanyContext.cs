c20142fef38ef240c5bca006ff3ebd8f0daf911a
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-11-05 11:22:55 +0330

    remove methods

--- DatabaseApi/Contexts/BaseCompanyContext.cs
+++ DatabaseApi/Contexts/BaseCompanyContext.cs
@@ -11,34 +11,5 @@ namespace DatabaseApi.Contexts
         protected BaseCompanyContext(Dictionary<string, string> configs)
         {
         }
-        public Dictionary<string, Company> GetAllCompaniesInfo()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public int GetCompaniesCount()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public List<string> GetCompaniesName()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public int GetFiscalYearCount(string companyName)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Dictionary<string, Company> GetFiscalYears(string companyName)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public (string, Company) GetLastFiscalYear(string companyName)
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
