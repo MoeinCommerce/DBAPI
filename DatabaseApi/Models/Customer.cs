2731d47f98c34a2d14b8732483c094aaddcb3a37
Erfan4708 <abbasy.4708@gmail.com>
2024-11-23 14:14:09 +0330

    Create customer model

--- DatabaseApi/Models/Customer.cs
+++ DatabaseApi/Models/Customer.cs
@@ -1,16 +1,19 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace DatabaseApi.Models
 {
-    public class Person : MappableModel
+    public class Customer : MappableModel
     {
+        public int? Code { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
-        public DateTime CreatedDate { get; } = DateTime.UtcNow;
-        public int Age => DateTime.UtcNow.Year - DateOfBirth.Year;
+        public DateTime CreatedDate { get; set; }
+        public List<string> PhoneNumbers { get; set; }
     }
+
 }
