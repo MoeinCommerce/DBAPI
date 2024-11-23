2fe492ba096312c73cea53a709b8bac9c38ab2e8
Erfan4708 <abbasy.4708@gmail.com>
2024-11-23 14:14:23 +0330

    Create invoice model

--- DatabaseApi/Models/Invoice.cs
+++ DatabaseApi/Models/Invoice.cs
@@ -1,12 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 namespace DatabaseApi.Models
 {
     public class Invoice : MappableModel
     {
         public int PersonId { get; set; }
-        public int WarehouseId { get; set; }
+        public string Status { get; set; }
         public DateTime InvoiceDate { get; set; }
+        public Billing Billing { get; set; }
+        public Shipping Shipping { get; set; }
         public decimal TotalAmount { get; set; }
+        public decimal TotalDiscount { get; set; }
+        public decimal TotalTax { get; set; }
+        public decimal TotalTaxDiscount { get; set; }
+        public List<InvoiceItem> InvoiceItems { get; set; }
     }
 }
