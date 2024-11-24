e29f5c3c56e4a778f9f82e868a043c1ff144373b
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-11-24 14:44:43 +0330

    change some variablies of invoice part

--- DatabaseApi/Models/Invoice.cs
+++ DatabaseApi/Models/Invoice.cs
@@ -5,15 +5,69 @@ namespace DatabaseApi.Models
 {
     public class Invoice : MappableModel
     {
+        // Foreign Key
         public int PersonId { get; set; }
+
+        // Invoice Items
+        public List<InvoiceItem> InvoiceItems { get; set; }
+
+        // Invoice Details
         public string Status { get; set; }
         public DateTime InvoiceDate { get; set; }
         public Billing Billing { get; set; }
         public Shipping Shipping { get; set; }
-        public decimal TotalAmount { get; set; }
-        public decimal TotalDiscount { get; set; }
-        public decimal TotalTax { get; set; }
-        public decimal TotalTaxDiscount { get; set; }
-        public List<InvoiceItem> InvoiceItems { get; set; }
+
+        // Base Properties
+        public double InvoiceDiscount { get; set; }
+        public double InvoiceTax { get; set; }
+        public double CarryPrice { get; set; }
+
+        // Computational Properties
+        public double SubTotal 
+        { 
+            get
+            {
+                double subTotal = 0;
+                foreach (var invoiceItem in InvoiceItems)
+                {
+                    subTotal += invoiceItem.SubTotal;
+                }
+
+                return subTotal;
+            } 
+        }
+
+        public double ItemsDiscount
+        { 
+            get
+            {
+                double itemsDiscount = 0;
+                foreach (var invoiceItem in InvoiceItems)
+                {
+                    itemsDiscount += invoiceItem.Discount;
+                }
+
+                return itemsDiscount;
+            }
+        }
+
+        public double Discount => ItemsDiscount + InvoiceDiscount;
+
+        public double Tax
+        {
+            get
+            {
+                double tax = InvoiceTax;
+                foreach (var invoiceItem in InvoiceItems)
+                {
+                    tax += invoiceItem.Tax;
+                }
+
+                return tax;
+            }
+        }
+
+        public double Total => SubTotal - Discount + Tax + CarryPrice;
+
     }
 }
--- DatabaseApi/Models/InvoiceItem.cs
+++ DatabaseApi/Models/InvoiceItem.cs
@@ -2,13 +2,21 @@ namespace DatabaseApi.Models
 {
     public class InvoiceItem
     {
+        // Foreign Keys
         public int InvoiceId { get; set; }
         public int ProductId { get; set; }
-        public int Quantity { get; set; }
         public int WarehouseId { get; set; }
-        public string RegularPrice { get; set; }
-        public string SubtotalTax { get; set; }
-        public string TotalTax { get; set; }
-        public string SalePrice { get; set; }
+
+        // Base Properties
+        public double UnitPrice { get; set; }
+        public double UnitDiscount { get; set; }
+        public double UnitTax { get; set; }
+        public int Quantity { get; set; }
+
+        // Computational Properties
+        public double SubTotal => Quantity * UnitPrice;
+        public double Discount => Quantity * UnitDiscount;
+        public double Tax => Quantity * UnitTax;
+        public double Total => SubTotal - Discount + Tax;
     }
 }
