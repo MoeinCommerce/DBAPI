1f7509f5bad010349eea4d1fa7eb17657dfc6e98
Erfan4708 <abbasy.4708@gmail.com>
2024-11-30 17:30:46 +0330

    Add some property into PaymentMethod model

--- DatabaseApi/Models/PaymentMethod.cs
+++ DatabaseApi/Models/PaymentMethod.cs
@@ -2,6 +2,9 @@ namespace DatabaseApi.Models
 {
     public class PaymentMethod : MappableModel
     {
-        
+        public string BankName { get; set; }
+        public string Branch { get; set; }
+        public string AccountNumber { get; set; }
+        public string CardNumber { get; set; }
     }
 }
