a55086320bff65df5fe979a7673dc3032ede04f7
Erfan4708 <abbasy.4708@gmail.com>
2024-12-29 09:05:09 +0330

    Remove IsActivated and Rank properties from warehouse method

--- DatabaseApi/Models/Warehouse.cs
+++ DatabaseApi/Models/Warehouse.cs
@@ -4,7 +4,5 @@ namespace DatabaseApi.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public bool IsActivated { get; set; }
-        public int Rank { get; set; }
     }
 }
