304203431d69d35db67e496e9302b0fa7abe6d74
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2025-02-15 10:14:07 +0330

    create variable product models

---
+++ DatabaseApi/Models/CollectionModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseApi.Models
+{
+    public class CollectionModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TargetWebId { get; set; }
+        public ObservableCollection<AttributeModel> Attributes { get; set; }
+    }
+}
