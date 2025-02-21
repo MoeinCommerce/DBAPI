7ddfd4a41d466d7728c1701e0e667d29e581486a
Erfan4708 <abbasy.4708@gmail.com>
2025-02-21 19:29:32 +0330

    Update DbConfig model

--- DatabaseApi/Models/DbConfig.cs
+++ DatabaseApi/Models/DbConfig.cs
@@ -5,5 +5,10 @@ namespace DatabaseApi.Models
         public string Key { get; set; }
         public string DefaultValue { get; set; }
         public string DisplayName { get; set; }
+        public string Value { get; set; }
+        public bool IsRequired { get; set; }
+        public bool IsReadOnly { get; set; }
+        public bool IsProtected { get; set; }
+        public string Description { get; set; }
     }
 }
