ab1401407e994c9741119a2bc8e37bdeec0fa29c
Erfan4708 <abbasy.4708@gmail.com>
2025-04-09 09:00:21 +0330

    feat: add addintional fields into Customer model

--- DatabaseApi/Models/Customer.cs
+++ DatabaseApi/Models/Customer.cs
@@ -7,10 +7,18 @@ namespace DatabaseApi.Models
 {
     public class Customer : MappableModel
     {
+        public int? GroupId { get; set; }
         public int? Code { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string FullName => FirstName + " " + LastName;
+        public string NationalCode { get; set; }
+        public string PostCode { get; set; }
+        public string AccountNumber { get; set; }
+        public string CompanyName { get; set; }
+        public string WebSite { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
