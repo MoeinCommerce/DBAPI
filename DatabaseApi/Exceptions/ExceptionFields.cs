511763fdcc4e9971fb63b617097f021362d8282f
Erfan4708 <abbasy.4708@gmail.com>
2025-07-06 10:04:35 +0330

    feat: add ExceptionFields class for standardized exception messages

---
+++ DatabaseApi/Exceptions/ExceptionFields.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseApi.Exceptions
+{
+    public static class ExceptionFields
+    {
+        public const string DuplicateCategoryName = "DuplicateCategoryName";
+        public const string InvalidCategoryId = "InvalidCategoryId";
+        public const string InvalidCategoryName = "InvalidCategoryName";
+        public const string MissingParentCategoryId = "MissingParentCategoryId";
+        public const string InvalidProductId = "InvalidProductId";
+        public const string InvalidProductCode = "InvalidProductCode";
+        public const string InvalidProductNameLength = "InvalidProductNameLength";
+        public const string InvalidProductDescriptionLength = "InvalidProductDescriptionLength";
+        public const string InvalidQuantity = "InvalidQuantity";
+        public const string InvalidPrice = "InvalidPrice";
+        public const string InvalidProductCategory = "InvalidProductCategory";
+        public const string InvoiceItemNotFound = "InvoiceItemNotFound";
+        public const string WarehouseIdNotFound = "WarehouseIdNotFound";
+        public const string BankNotFound = "BankNotFound";
+        public const string CustomerNotFound = "CustomerNotFound";
+        public const string InvalidInvoiceItemQuantity = "InvalidInvoiceItemQuantity";
+        public const string InvalidInvoiceItemPrice = "InvalidInvoiceItemPrice";
+        public const string InvalidTotalInvoicePrice = "InvalidTotalInvoicePrice";
+    }
+}
