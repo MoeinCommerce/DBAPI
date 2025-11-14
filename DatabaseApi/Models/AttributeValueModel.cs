6ec140dbb043eefa2f531e3b2a77b52615983072
Erfan4708 <abbasy.4708@gmail.com>
2025-11-14 14:46:16 +0330

    Implement INotifyPropertyChanged in AttributeValueModel
    Updated the AttributeValueModel class to implement the INotifyPropertyChanged interface.
    Refactored properties to use private backing fields and added logic to raise PropertyChanged events
    when property values change for Id, AttributeId, Value, and IsSelected.
    Included the OnPropertyChanged method to support property change notifications.

--- DatabaseApi/Models/AttributeValueModel.cs
+++ DatabaseApi/Models/AttributeValueModel.cs
@@ -1,11 +1,71 @@
-﻿namespace DatabaseApi.Models
+﻿using System.ComponentModel;
+
+namespace DatabaseApi.Models
 {
-    public class AttributeValueModel
+    public class AttributeValueModel : INotifyPropertyChanged
     {
-        public int Id { get; set; }
-        public int AttributeId { get; set; }
-        // public int ValueId { get; set; }
-        public string Value { get; set; }
-        public bool IsSelected { get; set; } = true;
+        private int _id;
+        private int _attributeId;
+        private string _value;
+        private bool _isSelected = true;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged(nameof(Id));
+                }
+            }
+        }
+
+        public int AttributeId
+        {
+            get => _attributeId;
+            set
+            {
+                if (_attributeId != value)
+                {
+                    _attributeId = value;
+                    OnPropertyChanged(nameof(AttributeId));
+                }
+            }
+        }
+
+        public string Value
+        {
+            get => _value;
+            set
+            {
+                if (_value != value)
+                {
+                    _value = value;
+                    OnPropertyChanged(nameof(Value));
+                }
+            }
+        }
+
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    OnPropertyChanged(nameof(IsSelected));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
