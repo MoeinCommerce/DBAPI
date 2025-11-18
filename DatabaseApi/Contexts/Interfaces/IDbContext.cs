ffc8a65cf5d8b234b090ead8cbbcc68ffab515d4
Erfan4708 <abbasy.4708@gmail.com>
2025-11-18 08:49:15 +0330

    change entry of GetProductStocksByWarehouseIds method from string to product model

--- DatabaseApi/Contexts/Interfaces/IDbContext.cs
+++ DatabaseApi/Contexts/Interfaces/IDbContext.cs
@@ -256,7 +256,7 @@ namespace DatabaseApi.Contexts.Interfaces
         /// <param name="prodcutId">Id of product</param>
         /// <param name="warehouseIds">List of warehouse id</param>
         /// <returns>List of tuples include warehouse id and stock of that warehouse.</returns>
-        List<(int WarehouseId, double Stock)> GetProductStocksByWarehouseIds(string productId, List<int> warehouseIds);
+        List<(int WarehouseId, double Stock)> GetProductStocksByWarehouseIds(Product product, List<int> warehouseIds);
 
         void SetProductsUpdateStatus(int targetWeb, bool updateRequired);
 
